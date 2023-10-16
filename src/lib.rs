07146177fe5f2f7660e71831a4dac6a06c8552c5
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-10-16 23:15:11 +0300

    Update `pyo3` dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 use pyo3::prelude::*;
 
+#[pyfunction]
+fn read_data_py(
+    _py: Python,
+    _path: &str,
+    _step_size: usize,
+    _max_lines: Option<usize>,
+) -> PyResult<()> {
+    todo!()
+}
+
 #[pymodule]
 fn anomrank_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    #[pyfn(m, "read_data")]
-    fn read_data_py(
-        _py: Python,
-        _path: &str,
-        _step_size: usize,
-        _max_lines: Option<usize>,
-    ) -> PyResult<()> {
-        todo!()
-    }
-
+    m.add_function(wrap_pyfunction!(read_data_py, m)?)?;
     Ok(())
 }
